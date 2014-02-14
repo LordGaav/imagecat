465a9c6cfddbf939354ebb74450d349c8b2895c1
Nick Douma <n.douma@nekoconeko.nl>
2014-02-14 23:04:07 +0100

    Create dir structure, and add license information

--- imagecat/image.py
+++ imagecat/image.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python
+# 
+# Copyright (c) 2014 Nick Douma < n.douma [at] nekoconeko . nl >
+# 
+# This file is part of imagecat.
+# 
+# imagecat is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# imagecat is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with imagecat. If not, see <http://www.gnu.org/licenses/>.
+#
 
 # I want PROPER divisions, please
 from __future__ import division
@@ -50,9 +67,3 @@ def montage(images, direction):
 			pointer += image.size[1]
 	
 	return canvas
-
-
-image1 = Image.open("081.jpg")
-image2 = Image.open("084.jpg")
-
-montage([cropresize(image1, (1920, 1080)), cropresize(image2, (1920, 1080))], HORIZONTAL).save("canvas.png")
